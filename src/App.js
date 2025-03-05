94c5cf0f4f1592bf1d8f2559bf090a4553081b3b
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-03-05 09:58:08 -0500

    feat(app): make tasks appear in in progress when added and completed section when marked complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Input from "./components/input/input";
+import { useState } from "react";
 function App() {
+  const [task, setTask] = useState([]);
+  const [completed, setCompleted] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const addTask = () => {
+    if (!inputValue) return;
+
+    const toDo = {
+      id: Date.now(),
+      text: inputValue,
+      completed: completed,
+    };
+
+    setTask((prevTask) => [...prevTask, toDo]);
+    setInputValue("");
+  };
+
+  const completeTask = (id) => {
+    setTask(
+      task.map((task) => {
+        if (id === task.id) {
+          return {
+            id: task.id,
+            text: task.text,
+            completed: true,
+          };
+        }
+
+        return task;
+      })
+    );
+  };
+
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Todos</h1>
+        <Input
+          addTask={addTask}
+          inputValue={inputValue}
+          handleChange={handleChange}
+          setInputValue={setInputValue}
+        />
       </header>
+      <main className="todo">
+        <div className="in-progress">
+          <p className="title">In Progress:</p>
+          {task
+            .filter((task) => task.completed === false) // Filter tasks that are not completed
+            .map((task) => (
+              <div className="task-section" key={task.id}>
+                <span>
+                  <p className="task">{task.text}</p>
+                </span>
+                <div className="complete-box">
+                  <p>Completed:</p>
+                  <input
+                    type="checkbox"
+                    onClick={() => completeTask(task.id)} // Mark the task as completed
+                  />
+                </div>
+              </div>
+            ))}
+        </div>
+
+        <div className="completed">
+          <p className="title">Complete:</p>
+          {task
+            .filter((task) => task.completed === true) // Filter tasks that are completed
+            .map((task) => (
+              <div className="task-section" key={task.id}>
+                <span>
+                  <p className="task">{task.text}</p>
+                </span>
+              </div>
+            ))}
+        </div>
+      </main>
     </div>
   );
 }
